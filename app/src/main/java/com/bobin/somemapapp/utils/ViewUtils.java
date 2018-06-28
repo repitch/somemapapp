b612306ba06c7c1a25395561adb87beef6225da7
nikit <nikitosbobin@gmail.com>
2018-06-28 16:12:58 +0500

    new util

--- app/src/main/java/com/bobin/somemapapp/utils/ViewUtils.java
+++ app/src/main/java/com/bobin/somemapapp/utils/ViewUtils.java
@@ -2,6 +2,8 @@ package com.bobin.somemapapp.utils;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.text.Html;
 import android.text.Spanned;
 import android.view.View;
@@ -12,7 +14,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 
 // part of com.material.components project
 public final class ViewUtils {
@@ -63,6 +69,14 @@ public final class ViewUtils {
 
     public static Spanned toHtml(String html) {
         return Html.fromHtml(html);
+    }
+
+    public static void glideRoundImage(ImageView imageView, String url, RequestListener<Drawable> listener) {
+        Glide.with(imageView)
+                .load(url)
+                .apply(new RequestOptions().circleCrop())
+                .listener(listener)
+                .into(imageView);
     }
 
     public static void glideRoundImage(ImageView imageView, String url) {
