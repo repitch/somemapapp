259fe588e1daa91933a908cf58ebfe5a45ecf3cb
nikit <nikitosbobin@gmail.com>
2018-07-03 00:17:46 +0500

    Удалил лишние тесты

--- app/src/androidTest/java/com/bobin/somemapapp/PointWatchedServiceImplTest.java
+++ app/src/androidTest/java/com/bobin/somemapapp/PointWatchedServiceImplTest.java
@@ -10,11 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.HashSet;
-
-import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
@@ -45,34 +41,5 @@ public class PointWatchedServiceImplTest {
         boolean watched = watchedService.isWatched(id);
 
         assertFalse(watched);
-    }
-
-    @Test
-    public void isWatchedForMultipleIdsWhenNotSaved() {
-        String id1 = DataGenerator.randomString();
-        String id2 = DataGenerator.randomString();
-
-        HashSet<String> watched = watchedService.isWatched(DataGenerator.asList(id1, id2));
-
-        assertNotNull(watched);
-        assertEquals(0, watched.size());
-    }
-
-    @Test
-    public void isWatchedForMultipleIds() {
-        String id1 = DataGenerator.randomString();
-        String id2 = DataGenerator.randomString();
-        String id3 = DataGenerator.randomString();
-        watchedService.setWatched(id1);
-        watchedService.setWatched(id2);
-
-        HashSet<String> watched = watchedService.isWatched(DataGenerator.asList(id1, id2, id3));
-
-        assertNotNull(watched);
-        assertEquals(2, watched.size());
-
-        assertTrue(watched.contains(id1));
-        assertTrue(watched.contains(id2));
-        assertFalse(watched.contains(id3));
     }
 }
